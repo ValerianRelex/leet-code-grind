7847433cbc5da248087efca365dcfbd6c964918c
swvalerian <valera.panafidin@teamgetter.com>
2024-11-13 18:22:31 +0900

    init solution, first example - need test

---
+++ src/main/java/swvalerian/validparentheses/ValidParentheses.java
@@ -0,0 +1,70 @@
+package swvalerian.validparentheses;
+
+import java.util.Stack;
+
+/**
+ * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+ * <p>
+ * An input string is valid if:
+ * <p>
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * Every close bracket has a corresponding open bracket of the same type.
+ * <p>
+ * <p>
+ * Example 1:
+ * Input: s = "()"
+ * Output: true
+ * <p>
+ * Example 2:
+ * Input: s = "()[]{}"
+ * Output: true
+ * <p>
+ * Example 3:
+ * Input: s = "(]"
+ * Output: false
+ * <p>
+ * Example 4:
+ * Input: s = "([])"
+ * Output: true
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= s.length <= 104
+ * s consists of parentheses only '()[]{}'.
+ */
+
+public class ValidParentheses {
+    public static boolean isValid(String s) {
+        char[] charArray = s.toCharArray();
+
+        char roundBracket = '(';
+        char cubBracket = '[';
+        char rocketBracket = '{';
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : charArray) {
+
+            if (ch == roundBracket) {
+                stack.push(')');
+            } else {
+                if (ch == cubBracket) {
+                    stack.push(']');
+                } else
+                    if (ch == rocketBracket) {
+                    stack.push('}');
+                }
+
+                if (!stack.isEmpty() && stack.peek() == ch) {
+                    stack.pop(); // удалим элемент
+                } else {
+                    return false;
+                }
+
+            }
+        }
+        return true;
+    }
+}
