480fca054875ea7eaa7ecbf9e649ae8f5d5b350a
swvalerian <valera.panafidin@teamgetter.com>
2024-11-21 21:18:47 +0900

    refactor algos and add tests

--- src/main/java/swvalerian/mergetwosortedlists/MergeTwoSortedLists.java
+++ src/main/java/swvalerian/mergetwosortedlists/MergeTwoSortedLists.java
@@ -4,22 +4,22 @@ package swvalerian.mergetwosortedlists;
  * You are given the heads of two sorted linked lists list1 and list2.
  * Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
  * Return the head of the merged linked list.
- *
+ * <p>
  * Example 3:
  * Input: list1 = [1,2,4], list2 = [1,3,4]
  * Output: [1,1,2,3,4,4]
- *
+ * <p>
  * Example 2:
  * Input: list1 = [], list2 = []
  * Output: []
- *
+ * <p>
  * Example 3:
  * Input: list1 = [], list2 = [0]
  * Output: [0]
  */
 public class MergeTwoSortedLists {
 
-    public class ListNode {
+    public static class ListNode {
         int val;
         ListNode next;
 
@@ -34,47 +34,27 @@ public class MergeTwoSortedLists {
             this.val = val;
             this.next = next;
         }
+    }
 
-        public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-            if (list1 == null) {
-                return list2;
-            } else if (list2 == null) {
-                return list1;
-            }
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode head = new ListNode();
+        ListNode mergedList = head;
 
-            // на данном этапе мы четко знаем, что оба наших списка не пустые.
-            // теперь нам надо инициализировать mergedList, чтобы сохранить на него ссылку в head
+        while (list1 != null && list2 != null) {
 
-            ListNode mergedList;
-
-            if (list1.val < list2.val) {
-                mergedList = list1;
+                if (list1.val < list2.val) {
+                mergedList.next = list1;
                 list1 = list1.next;
             } else {
-                mergedList = list2;
+                mergedList.next = list2;
                 list2 = list2.next;
             }
 
-            ListNode head = mergedList;
-
-            while (list1 != null && list2 != null) {
-
-                if (list1 == null) {
-                    mergedList.next = new ListNode(list2.val);
-                    list2 = list2.next;
-                } else if (list2 == null) {
-                    mergedList.next = new ListNode(list1.val);
-                    list1 = list1.next;
-                } else if (list1.val < list2.val) {
-                    mergedList.next = new ListNode(list1.val);
-                    list1 = list1.next;
-                } else {
-                    mergedList = new ListNode(list2.val);
-                    list2 = list2.next;
-                }
-            }
-
-            return head;
+            mergedList = mergedList.next;
         }
+
+        mergedList.next = list1 == null ? list2 : list1;
+
+        return head.next;
     }
 }
---
+++ src/test/java/swvalerian/mergetwosortedlists/MergeTwoSortedListsTest.java
@@ -0,0 +1,63 @@
+package swvalerian.mergetwosortedlists;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * You are given the heads of two sorted linked lists list1 and list2.
+ * Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+ * Return the head of the merged linked list.
+ *
+ * Example 3:
+ * Input: list1 = [1,2,4], list2 = [1,3,4]
+ * Output: [1,1,2,3,4,4]
+ *
+ * Example 2:
+ * Input: list1 = [], list2 = []
+ * Output: []
+ *
+ * Example 3:
+ * Input: list1 = [], list2 = [0]
+ * Output: [0]
+ */
+class MergeTwoSortedListsTest {
+
+    @Test
+    void mergeTwoSortedLists_case1() {
+        MergeTwoSortedLists.ListNode list1 = new MergeTwoSortedLists.ListNode(1);
+        MergeTwoSortedLists.ListNode list2 = new MergeTwoSortedLists.ListNode(1);
+
+        list1.next = new MergeTwoSortedLists.ListNode(2, new MergeTwoSortedLists.ListNode(4));
+        list2.next = new MergeTwoSortedLists.ListNode(3, new MergeTwoSortedLists.ListNode(4));
+
+        MergeTwoSortedLists.ListNode mergedList = MergeTwoSortedLists.mergeTwoLists(list1, list2);
+
+        assertEquals(1, mergedList.val);
+        assertEquals(1, mergedList.next.val);
+        assertEquals(2, mergedList.next.next.val);
+        assertEquals(3, mergedList.next.next.next.val);
+        assertEquals(4, mergedList.next.next.next.next.val);
+        assertEquals(4, mergedList.next.next.next.next.next.val);
+    }
+
+    @Test
+    void mergeTwoSortedLists_case2() {
+
+    }
+
+    @Test
+    void mergeTwoSortedLists_case3() {
+
+    }
+
+    @Test
+    void mergeTwoSortedLists_case4() {
+
+    }
+
+    @Test
+    void mergeTwoSortedLists_case5() {
+
+    }
+}
